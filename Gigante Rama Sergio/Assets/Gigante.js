1f477c4731f2a10b772e99017db5822bbdf30ce5
Francisco Sucre <frank91frank@gmail.com>
2015-09-23 15:15:48 -0430

    Inicializacion

---
+++ Gigante Rama Sergio/Assets/Gigante.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var movimientoIzq: KeyCode;
+var movimientoDer: KeyCode;
+var velocidad: int;
+ 
+function Start () {
+		
+}
+
+function Update () {
+
+	if(Input.GetKey(movimientoIzq)){
+		GetComponent.<Rigidbody2D>().velocity.x = -velocidad;
+	}
+	
+	if(Input.GetKey(movimientoDer)){
+		GetComponent.<Rigidbody2D>().velocity.x = velocidad;
+	}
+}
---
+++ Gigante Rama Sergio/Assets/Scrypts/Gigante.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gigante : MonoBehaviour
+{
+	public KeyCode movimientoIzq;
+	public KeyCode movimientoDer;
+	public float velocidad;
+	private Rigidbody2D gigante;
+
+	void Start () {
+		gigante = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+
+		Vector2 tempVelocity;
+		tempVelocity = gigante.velocity;
+
+		if(Input.GetKey(movimientoIzq)){
+			tempVelocity.x = velocidad;
+			gigante.velocity = tempVelocity;
+		}
+		
+		if(Input.GetKey(movimientoDer)){
+			tempVelocity.x = -velocidad;
+			gigante.velocity = tempVelocity;
+		}
+	}
+}
